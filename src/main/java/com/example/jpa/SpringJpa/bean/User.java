f4a195877cf140064da356b8261d13f8f4a24c25
CedricP-Git <61793442+CedricP-Git@users.noreply.github.com>
2020-03-11 19:50:24 +0100

    2nd commit wesh

--- src/main/java/com/example/jpa/SpringJpa/bean/User.java
+++ src/main/java/com/example/jpa/SpringJpa/bean/User.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
-
+//coucou je tente un truc
 @Entity
 public class User implements Serializable{
     @Id
